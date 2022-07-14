364d3ce178fbe08dd29e706abc61522f65f6982d
Karl Lundqvist <karl.lundqvist0@gmail.com>
2022-07-14 13:38:32 +0200

    Struggle continues

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const mainContainer = document.querySelector('#main');
+var fidelity = 96;
 
+const mainContainer = document.querySelector('#main');
+const setBtn = document.querySelector('#setBtn');
 
 
 function makeDiv() {
@@ -13,14 +15,30 @@ function makeDiv() {
 }
 
 
-for (let i=0; i<256; i++){
-    makeDiv();  
-}
 
-const content = document.querySelectorAll('#content');
+
+const content = document.querySelectorAll(".content");
+
+setBtn.addEventListener("click", function(){
+    let fidelity = prompt("Enter a number between 1 and 100", "96");
+        if(fidelity < 1 || fidelity > 100 || fidelity == NaN){
+            alert("Invalid input")
+            location.reload();
+        }
+        else{
+            console.log(fidelity);
+            document.querySelectorAll(".content").forEach(content => {content.style.width = fidelity/9.6});
+            }
+            
+    } )
+
+for (let i=0; i<fidelity*fidelity; i++){
+        makeDiv();  
+    }
 
 
 
 console.log(mainContainer);
 console.log(content);
+console.log(fidelity);
 
