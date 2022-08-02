478647f401d7fa4eade0bf68320821b309365eef
Karl Lundqvist <karl.lundqvist0@gmail.com>
2022-08-02 11:19:27 +0200

    Made it work

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
-var fidelity = 96;
+
 
 const mainContainer = document.querySelector('#main');
 const setBtn = document.querySelector('#setBtn');
+var fidelity = 64;
 
+makeGrid(fidelity);
 
-function makeDiv() {
+// makes a div with a given size
+function makeDiv(size) {
     var contentDiv = document.createElement('div');
     contentDiv.className = 'content';
+    contentDiv.style.width = `${size}px`;
     contentDiv.addEventListener("mouseover", function(){
         this.classList.add("active");
-    })
-    mainContainer.appendChild(contentDiv);
+    });
+    return contentDiv;
+}
 
+// function for putting contentDivs into the grid
+function makeGrid(fidelity){
+    for (let i=0; i<fidelity*fidelity; i++){
+        mainContainer.appendChild(makeDiv(960 / fidelity))  
+    }
 }
 
 
-
-
-const content = document.querySelectorAll(".content");
-
 setBtn.addEventListener("click", function(){
-    let fidelity = prompt("Enter a number between 1 and 100", "96");
+    let fidelity = parseInt(prompt("Enter a number between 1 and 100", "64"));
         if(fidelity < 1 || fidelity > 100 || fidelity == NaN){
             alert("Invalid input")
             location.reload();
         }
         else{
+            while (mainContainer.firstChild){
+                mainContainer.removeChild(mainContainer.lastChild);
+            }
+
+
             console.log(fidelity);
-            content.forEach(element => {element.style.width = parseInt(fidelity)/9.6+'px'}, console.log('hello'));
+            makeGrid(fidelity);
+            console.log(typeof(fidelity));
             }
             
     } )
 
-for (let i=0; i<fidelity*fidelity; i++){
-        makeDiv();  
-    }
-
-
-
-console.log(mainContainer);
-console.log(content);
-console.log(fidelity);
 
